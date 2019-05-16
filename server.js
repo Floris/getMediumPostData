8e88f3a4048ce6e0faedfad17850694d7923ce49
Floris Droppert <Floris@floris-2.local>
2019-05-16 14:15:22 +0200

    update

--- server.js
+++ server.js
@@ -9,28 +9,35 @@ const rp = require('request-promise');
 app.use(bodyParser.json());
 
 // CORSS
-// app.use(function (req, res, next) {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//     // res.setHeader('Access-Control-Request-Headers', 'x-auth, Content-Type');
-//     // res.setHeader('Access-Control-Request-Method', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     next();
-// });
-
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    // res.setHeader('Access-Control-Request-Headers', 'x-auth, Content-Type');
+    // res.setHeader('Access-Control-Request-Method', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     next();
 });
+
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Credentials", "true")
+//     next();
+// });
 
 app.get('/', (req, res) => {
     // const url = 'https://medium.com/@nebulasio/latest?format=json'; // test medium url
 
     const body = _.pick(req.body, ['url']);
-    const url = body.url;
+    // const url = body.url;
+    const url = req.query.url;
+
+    console.log('url', url);
+    console.log(req.query.url)
 
     if (!url) {
         console.log('no url');
@@ -39,16 +46,20 @@ app.get('/', (req, res) => {
         return;
     }
 
-    rp(url)
-        .then(function (html) {
-            const parsed_json = JSON.parse(html.substring(16)); //success!
-            // console.log(html);
-            res.send(parsed_json)
-        })
-        .catch(function (err) {
-            // res.send('error'); //handle error
-            res.status(500).send('Error getting data');
-        });
+    try {
+        rp(url)
+            .then(function (html) {
+                const parsed_json = JSON.parse(html.substring(16)); //success!
+                // console.log(html);
+                res.send(parsed_json);
+            })
+            .catch(function (err) {
+                // res.send('error'); //handle error
+                res.status(500).send('Error getting data');
+            });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
